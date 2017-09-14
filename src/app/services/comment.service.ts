2e6870f91ed257f9233136dedabc43608260232b
Tariqul Islam <tariqul@itconquest.com>
2017-09-14 19:07:29 +0600

    Add Service Obserable to service

---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http'
+import { Comment } from '../models/comment'
+import 'rxjs'
+import { Observable } from 'rxjs/Rx'
+
+
+@Injectable()
+export class CommentService {
+
+  constructor() { }
+
+}
