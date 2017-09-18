fd24c8de8327105715e5fd061d67d66969d7a3c5
Tariqul Islam <tariqul@itconquest.com>
2017-09-18 18:13:33 +0600

    Add Service module

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -7,7 +7,41 @@ import { Observable } from 'rxjs/Rx'
 
 @Injectable()
 export class CommentService {
+  
+  constructor(private http:Http) {}
+  
+  private commentsUrl = 'http://localhost:3000/api/comments'
 
-  constructor() { }
+  getComments() : Observable<Comment[]> {
+    return this.http.get(this.commentsUrl)
+                    .map((res:Response) => res.json())
+                    .catch((error:any) => Observable.throw(error.json().error || 'Server Error'))
+  }
 
+  addComment(body: Object) : Observable<Comment[]> {
+    let bodyString = JSON.stringify(body)
+    let headers = new Headers({'Content-Type': 'application/json'})
+    let options = new RequestOptions({headers: headers})
+
+    return this.http.post(this.commentsUrl, body,options)
+                    .map((res:Response) => res.json())
+                    .catch((error:any) => Observable.throw(error.json().error || 'Server Error'))
+  }
+
+  updateComment(body:Object): Observable<Comment[]> {
+    let bodyString = JSON.stringify(body)
+    let headers = new Headers({'Content-Type': 'application/json'})
+    let options = new RequestOptions({ headers:headers })
+
+    return this.http.put(`${this.commentsUrl}/${body['id']}`, body, options)
+                    .map((res:Response) => res.json())
+                    .catch((error:any) => Observable.throw(error.json().error || 'Server Error'))
+  }
+
+  removeComment(id:Object): Observable<Comment[]> {
+    return this.http.delete(`${this.commentsUrl}/${id}`)
+                    .map((res:Response) => res.json())
+                    .catch((error:any) => Observable.throw(error.json().error || 'Server Error'))
+
+  }
 }
