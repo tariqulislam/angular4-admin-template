bafb973fba05e1f6fc456caac4bf3cb4d17bcb77
Tariqul Islam <tariqul.islam.rony@gmail.com>
2018-06-01 19:20:32 +0600

    add user vault validation

--- src/app/controller/user/user-add/user-add.component.ts
+++ src/app/controller/user/user-add/user-add.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { User } from '../../../models/user.model';
+import { NgForm, ValidationErrors } from '@angular/forms'
 
 
 @Component({
@@ -11,15 +12,32 @@ export class UserAddComponent implements OnInit {
   @Output() userCreate:  EventEmitter<any> = new EventEmitter();
   @Input() message: string;
   @Input() statusType: string;
+
+  isValidFormSubmitted = null;
+  user = new User(0,'','','');
+
+ 
+
   constructor() { }
 
-  model = new User(0,'Tariqul','islam','');
 
   ngOnInit() {
+    
   }
 
-  onSubmit() {
-     this.userCreate.emit(this.model);
+  
+
+  onSubmit(form: NgForm) {
+    this.isValidFormSubmitted = false;
+      if(form.invalid) {
+        return;
+      }
+
+     this.isValidFormSubmitted = true;
+     this.user = form.value;
+     this.userCreate.emit(this.user);
+     this.user = new User(0,'','','');
+     form.resetForm()
   }
 
 }
